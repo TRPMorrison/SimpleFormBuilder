44b271a129174c2c7c3bb4d067ddcee0680cbe9f
Trini Morrison <TRPMorrison@gmail.com>
2015-05-26 12:18:22 -0400

    REVISED FILES TO ADD THE SCRIPT

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ var person = {
   location: 'Atlanta'
 }
 
-var templateString = 'Hi, my name is <%= name %> and I am from <%= location %>';
+var templateString = $('#personFormat').text();
+
+// SAMPE AS ABOVE:  var templateString = 'Hi, my name is <%= name %> and I am from <%= location %>';
 
 var templateFunction = _.template(templateString);
 
@@ -19,43 +21,43 @@ console.log(finalHTML);
 
 
 
-var form1 = [
-  { type: 'text', label: 'First Name' },
-  { type: 'text', label: 'Last Name' },
-  { type: 'email', label: 'Email' },
-  { type: 'password', label: 'Password' },
-  { type: 'password', label: 'Password Verify' },
-  { type: 'submit', label: 'Register'}
-];
+// var form1 = [
+//   { type: 'text', label: 'First Name' },
+//   { type: 'text', label: 'Last Name' },
+//   { type: 'email', label: 'Email' },
+//   { type: 'password', label: 'Password' },
+//   { type: 'password', label: 'Password Verify' },
+//   { type: 'submit', label: 'Register'}
+// ];
 
-function formBuilder (data) {
+// function formBuilder (data) {
 
-  var formHTML = '<form>';
+//   var formHTML = '<form>';
 
-  data.forEach( function (elem) {
+//   data.forEach( function (elem) {
 
-    if (elem.type === 'submit') {
+//     if (elem.type === 'submit') {
 
-      formHTML += '<input type="' + elem.type + '" value="' + elem.label + '" />';
+//       formHTML += '<input type="' + elem.type + '" value="' + elem.label + '" />';
 
-    } else {
+//     } else {
 
-      formHTML += '<label>' + elem.label + '</label>';
-      formHTML += '<input type="' + elem.type + '" />';
-      formHTML += '</br></br>';
+//       formHTML += '<label>' + elem.label + '</label>';
+//       formHTML += '<input type="' + elem.type + '" />';
+//       formHTML += '</br></br>';
 
-    }
+//     }
 
-  });
+//   });
 
-  formHTML += '</form>';
+//   formHTML += '</form>';
 
-  console.log(formHTML);
+//   console.log(formHTML);
 
-  $('#registrationForm').html(formHTML);
+//   $('#registrationForm').html(formHTML);
 
-}
+// }
 
-formBuilder(form1);
+// formBuilder(form1);
 
 
